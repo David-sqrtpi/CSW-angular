6e66d7e0572a0478f557f326ba89cb254523439d
William Alejandro Peres Perez <wperez90@uan.edu.co>
2021-11-15 23:28:50 -0500

    terminacion de caso de uso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InsertarEmpleadoComponent } from './components/insertar-empleado/insertar-empleado.component';
+import { CrearCuentaComponent } from './componentes/crear-cuenta/crear-cuenta.component';
 
 const routes: Routes = [
-  { path: 'insertar-empleado', component: InsertarEmpleadoComponent }
+  { path: 'insertar-empleado', component: InsertarEmpleadoComponent },
+  { path: 'crear-cuenta', component: CrearCuentaComponent }
+
 ];
 
 @NgModule({
